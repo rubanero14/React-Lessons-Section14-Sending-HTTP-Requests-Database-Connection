28b1399acf1bc5ca941489735421f3a290f37e1e
Rajaruban Rajindram <ahyat888@gmail.com>
2023-03-15 15:43:54 +0800

    Handling Loading & Data States

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import React, { useState } from "react";
 import axios from "axios";
 
+import Loading from "./components/Loading";
 import MoviesList from "./components/MoviesList";
 import "./App.css";
 
 function App() {
   const [dummyMovies, setDummyMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   const fetchMovies = async () => {
+    setIsLoading(true);
     const response = await axios.get("https://swapi.dev/api/films");
-    return setDummyMovies(response.data.results);
+    setDummyMovies(response.data.results);
+    return setIsLoading(false);
   };
 
   return (
@@ -17,11 +21,17 @@ function App() {
       <section>
         <button onClick={fetchMovies}>Fetch Movies</button>
       </section>
-      {dummyMovies.length > 0 && (
-        <section>
+      <section>
+        {isLoading && <Loading />}
+        {dummyMovies.length === 0 && !isLoading && (
+          <p className="mb-0 text-danger">
+            <strong>No movies found!</strong>
+          </p>
+        )}
+        {dummyMovies.length > 0 && !isLoading && (
           <MoviesList movies={dummyMovies} />
-        </section>
-      )}
+        )}
+      </section>
     </React.Fragment>
   );
 }
---
+++ src/components/Loading.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Loading = () => {
+  return <div class="spinner-border" role="status" />;
+};
+
+export default Loading;
