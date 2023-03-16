147a87ee6f9c408338e2e4d61367b3bfb78996fb
Rajaruban Rajindram <ahyat888@gmail.com>
2023-03-16 15:16:53 +0800

    Handling Http Errors

--- src/App.js
+++ src/App.js
@@ -8,30 +8,54 @@ import "./App.css";
 function App() {
   const [dummyMovies, setDummyMovies] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState("");
+  const [isError, setIsError] = useState(false);
 
   const fetchMovies = async () => {
     setIsLoading(true);
-    const response = await axios.get("https://swapi.dev/api/films");
-    setDummyMovies(response.data.results);
+    try {
+      setIsError(false);
+      const response = await axios.get("https://swapi.dev/api/films");
+      setDummyMovies(response.data.results);
+    } catch (err) {
+      setError(err.message);
+      setIsError(true);
+    }
     return setIsLoading(false);
   };
+
+  let content = "";
+
+  if (dummyMovies.length === 0 && !isLoading && !isError) {
+    content = (
+      <p className="mb-0 text-warning">
+        <strong>No movies found!</strong>
+      </p>
+    );
+  }
+
+  if (!isLoading && isError) {
+    content = (
+      <p className="mb-0 text-danger">
+        <strong>{error}!</strong>
+      </p>
+    );
+  }
+
+  if (dummyMovies.length > 0 && !isLoading && !isError) {
+    content = <MoviesList movies={dummyMovies} />;
+  }
+
+  if (isLoading) {
+    content = <Loading />;
+  }
 
   return (
     <React.Fragment>
       <section>
         <button onClick={fetchMovies}>Fetch Movies</button>
       </section>
-      <section>
-        {isLoading && <Loading />}
-        {dummyMovies.length === 0 && !isLoading && (
-          <p className="mb-0 text-danger">
-            <strong>No movies found!</strong>
-          </p>
-        )}
-        {dummyMovies.length > 0 && !isLoading && (
-          <MoviesList movies={dummyMovies} />
-        )}
-      </section>
+      <section>{content}</section>
     </React.Fragment>
   );
 }
